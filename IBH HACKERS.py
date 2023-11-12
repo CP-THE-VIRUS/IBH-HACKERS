151258e70bb42d8e4a73962c100e256e79409fcf
ᴄᴩ ᴛʜᴇ ᴠɪʀᴜꜱ <150131691+CP-THE-VIRUS@users.noreply.github.com>
2023-11-12 14:12:38 +0530

    Add files via upload

---
+++ IBH HACKERS.py
@@ -0,0 +1,41 @@
+from termcolor import colored
+print (colored("""
+
+IG = @jblfxe
+
+this repo for join our whatsapp group
+
+
+
+    ________  __  __   __  _____   ________ __ __________  _____
+   /  _/ __ )/ / / /  / / / /   | / ____/ //_// ____/ __ \/ ___/
+   / // __  / /_/ /  / /_/ / /| |/ /   / ,<  / __/ / /_/ /\__ \ 
+ _/ // /_/ / __  /  / __  / ___ / /___/ /| |/ /___/ _, _/___/ / 
+/___/_____/_/ /_/  /_/ /_/_/  |_\____/_/ |_/_____/_/ |_|/____/  
+                                                                                                                        
+                                                                                                                         
+                                                                                                         
+                                                                                                         
+          group link : https://chat.whatsapp.com/I0yR1oOu7unK6igGHpAsQV
+                                     
+                                      
+MAIN ADMIN : CP THE VIRUS
+ IG    : @jblfxe
+2ND  ADMIN :  SPY ROX 
+                                        
+                                                                                                        
+ A GROUP OF BLACK HAT HACKERS  
+                                                                                                                                                                  IROUS BLACK HAT HACKERS""" , 'blue'))
+                                                                                                                                 
+                                                                                                                                                           
+                                                                                                                                                                                                                                      
+                                                                                                          
+                                                                                                         
+                                                                                                         
+                                                                                                         
+                                                                                                         
+                                                                                                         
+                                                                                                                      
+
+                                                                                   
+                                                                                                                                                                                                                                                                                                                                                                                                      
